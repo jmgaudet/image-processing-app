ed7e3830f6da885dbe38c1bc69c7bd7075dde5d3
Jeremy Gaudet <jeremymgaudet@gmail.com>
2021-04-05 10:31:02 -0400

    setting up

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import cv2 as cv
+import matplotlib as plt
+import numpy as np
 import tensorflow as tf
+from skimage import data, io, filters
 
 
-def print_hi():
-    print(f'Hi, {tf.__version__}')
+def print_versions():
+    print(f'tensorflow: {tf.__version__}')
+    print(f'matplotlib: {plt.__version__}')
+    print(f'numpy: {np.__version__}')
+    print(f'opencv: {cv.__version__}')
+
+
+def draw_something():
+    image = data.coins()
+    edges = filters.sobel(image)
+    io.imshow(edges)
+    io.show()
 
 
 if __name__ == '__main__':
-    print_hi()
+    print_versions()
+    draw_something()
